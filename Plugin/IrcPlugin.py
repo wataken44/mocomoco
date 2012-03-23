3706a3264f746d77a4483f20c994a3b54d188174
wataken44 <twitter.com/wataken44>
2012-03-24 01:22:26 +0900

    IrcPlugin multithreaded

--- Plugin/IrcPlugin.py
+++ Plugin/IrcPlugin.py
@@ -11,37 +11,49 @@ from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol
 from twisted.python import log
 
-from threading import Thread
-
 # system imports
 import sys
+import datetime
 import time
 
+from threading import Thread, Lock
+
 class IrcPlugin(object):
+    def __init__(self):
+        pass
+
     def Initialize(self, runner, name, param):
         self.__name = name
         runner.AddProcessHook(self.Process)
 
-        self.__channel = param['Channel'].encode('utf-8')
-        if self.__channel[0] != "#":
-            self.__channel = "#" + self.__channel
+        # initialize message queue
+        runIntervalSec = 720
+        if 'RunIntervalSec' in param:
+            runInterval = int(param['RunIntervalSec'])
 
-        self.__factory = MocoBotFactory(self, self.__channel)
-        self.__bot = None
-        self.__server = param['Server']
-        
-        reactor.connectTCP(self.__server, 6667, self.__factory)
+        messageIntervalSec = 5
+        if 'MessageIntervalSec' in param:
+            messageInterval = int(param['MessageIntervalSec'])
 
-        self.__thread = TwistedThread()
-        runner.AddThread(self.__thread)
+        qt = QueueThread(runIntervalSec, messageIntervalSec)
+        self.__queue = qt
+        runner.AddThread(qt)
 
-    def RegisterBot(self, bot):
-        self.__bot = bot
-        
+        # initialize bot
+        server = param['Server']
+        port = 6667
+        if 'Port' in param:
+            port = int(param['Port'])
+        channel = param['Channel'].encode("utf-8")
+        if channel[0] != '#':
+            channel = "#" + channel
+
+        factory = MocoBotFactory(channel, qt)
+
+        tt = TwistedThread(server, port, factory)
+        runner.AddThread(tt)
+
     def Process(self, entry):
-        if self.__bot is None:
-            return
-
         msg = ["[%s]" % entry.sourceTitle, entry.title, entry.link]
 
         while len(msg) > 0:
@@ -59,7 +71,6 @@ class IrcPlugin(object):
                     msg.pop(0)
                     continue
                 break
-
                     
             if len(buf.encode("utf-8")) >= 480:
                 tmp = ""
@@ -69,61 +80,119 @@ class IrcPlugin(object):
                         break
                 buf = tmp
 
-            self.__bot.notice(self.__channel, buf.encode('utf-8'))
+            self.__queue.Enque(buf)
 
-class TwistedThread(Thread):
-    def __init__(self):
+class QueueThread(Thread):
+    def __init__(self, runIntervalSec, messageIntervalSec):
         Thread.__init__(self)
-        self.__finished = False
         Thread.setDaemon(self, True)
         
+        self.__queue = []
+        self.__lock = Lock()
+
+        self.__runIntervalSec = runIntervalSec
+        self.__messageIntervalSec = messageIntervalSec
+
+        self.__bot = None
+
+    def RegisterBot(self, bot):
+        self.__bot = bot
+
+    def Enque(self, message):
+        self._Lock()
+        self.__queue.append(message)
+        self._Unlock()
+
     def Start(self):
         Thread.start(self)
-        time.sleep(5)
 
     def Stop(self):
-        self.__finished = True
+        pass
+
+    def Join(self):
+        Thread.join(self)
+
+    def _Lock(self):
+        self.__lock.acquire()
+
+    def _Unlock(self):
+        self.__lock.release()
+
+    def run(self):
+        while True:
+            while self.__bot is not None:
+                msg = None
+
+                self._Lock()
+                if len(self.__queue) > 0:
+                    msg = self.__queue[0]
+                    self.__queue.pop(0)
+                self._Unlock()
+
+                if msg is None:
+                    break
+                else:
+                    self.__bot.Say(msg)
+               
+                time.sleep(self.__messageIntervalSec)
+
+            time.sleep(self.__runIntervalSec)
+
+class TwistedThread(Thread):
+    def __init__(self, server, port, factory):
+        Thread.__init__(self)
+        Thread.setDaemon(self, True)
+
+        self.__server = server
+        self.__port = port
+        self.__factory = factory
+
+    def Start(self):
+        Thread.start(self)
+
+    def Stop(self):
+        pass
 
     def Join(self):
         Thread.join(self)
 
     def run(self):
+        reactor.connectTCP(self.__server, self.__port, self.__factory)
         reactor.run(installSignalHandlers=0)
 
 class MocoBot(irc.IRCClient):
     nickname = "moco"
 
+    def __init__(self, channel, queue):
+        #irc.IRCClient.__init__(self)
+
+        self.__channel = channel
+        self.__queue = queue
+        
     def signedOn(self):
         """Called when bot has succesfully signed on to server."""
-        self.join(self.factory.channel)
+        self.join(self.__channel)
 
     def joined(self, channel):
-        self.factory.plugin.RegisterBot(self)
-        self.notice(self.factory.channel, (u"こんにちはこんにちは").encode('utf-8'))
-    
+        self.__queue.RegisterBot(self)
+        self.Say(u"こんにちはこんにちは")
+
+    def Say(self, msg):
+        self.say(self.__channel, msg.encode('utf-8'))
+
 class MocoBotFactory(protocol.ClientFactory):
-    def __init__(self, plugin, channel):
-        self.__plugin = plugin
+    def __init__(self, channel, queue):
+        self.__queue = queue
         self.__channel = channel
 
-    @property
-    def channel(self):
-        return self.__channel
-
-    @property
-    def plugin(self):
-        return self.__plugin
-
     def buildProtocol(self, addr):
-        p = MocoBot()
-        p.factory = self
-        return p
+        protocol = MocoBot(self.__channel, self.__queue)
+        return protocol
 
     def clientConnectionLost(self, connector, reason):
         """If we get disconnected, reconnect to server."""
         print "connection lost:", reason
         connector.connect()
-        #reactor.stop()
 
     def clientConnectionFailed(self, connector, reason):
         print "connection failed:", reason
